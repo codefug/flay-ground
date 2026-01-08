ff9baa287b4c10786f7fc7b1886e379467230198
codefug <robot9917@gmail.com>
2026-01-08 22:01:55 +0900

    feat: 레이아웃 및 페이지에 Provider 적용
    - RootLayout에 MSWProvider와 TRPCReactProvider 추가
    - 메인 페이지에 ApiTestPanel 컴포넌트 추가

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { MSWProvider } from "@/components/MSWProvider";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <MSWProvider>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </MSWProvider>
       </body>
     </html>
   );
